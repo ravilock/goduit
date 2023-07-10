f55c25b11d5e4d7e2df49990ff8bdbfa0b837c13
ravilock <ravi.me.professional@hotmail.com>
2023-07-10 20:11:14 -0300

    feat: add get current user function

--- api/responses/user.go
+++ api/responses/user.go
@@ -2,10 +2,10 @@ package responses
 
 type User struct {
 	User struct {
-		Username *string `json:"username"`
-		Email    *string `json:"email"`
-		Token    *string `json:"token"`
-		Bio      *string `json:"bio"`
-		Image    *string `json:"image"`
+		Username string `json:"username"`
+		Email    string `json:"email"`
+		Token    string `json:"token,omitempty"`
+		Bio      string `json:"bio,omitempty"`
+		Image    string `json:"image,omitempty"`
 	} `json:"user"`
 }
