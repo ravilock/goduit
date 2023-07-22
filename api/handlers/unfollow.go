88939193d5af4179fd1e5cba7304ca8a661beb14
ravilock <ravi.me.professional@hotmail.com>
2023-07-21 21:05:45 -0300

    feat: implement follow and unfollow handlers

---
+++ api/handlers/unfollow.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/ravilock/goduit/api/assemblers"
+	"github.com/ravilock/goduit/api/requests"
+	"github.com/ravilock/goduit/api/validators"
+	"github.com/ravilock/goduit/internal/app/services"
+)
+
+func Unfollow(c echo.Context) error {
+	clientUsername := c.Request().Header.Get("Goduit-Client-Username")
+	request := new(requests.Follower)
+
+	request.Username = c.Param("username")
+	if err := validators.Follower(request); err != nil {
+		return err
+	}
+
+	ctx := c.Request().Context()
+
+	err := services.Unfollow(request.Username, clientUsername, ctx)
+
+	dto, err := services.GetProfileByUsername(request.Username, ctx)
+	if err != nil {
+		return err
+	}
+	dto.Following = false
+
+	response := assemblers.ProfileResponse(dto)
+
+	return c.JSON(http.StatusOK, response)
+}
