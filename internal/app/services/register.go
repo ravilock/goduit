ace7b1995a762e1fc4fc9cba616d90a5f68865ff
ravilock <ravi.me.professional@hotmail.com>
2023-11-17 22:23:51 -0300

    ref: use identity package on login and registration

--- internal/app/services/register.go
+++ internal/app/services/register.go
@@ -1,48 +1,29 @@
 package services
 
 import (
-	"time"
-
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
-	"github.com/ravilock/goduit/internal/app/dtos"
 	"github.com/ravilock/goduit/internal/app/models"
 	"github.com/ravilock/goduit/internal/app/repositories"
-	encryptionkeys "github.com/ravilock/goduit/internal/config/encryptionKeys"
+	"github.com/ravilock/goduit/internal/identity"
 	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/net/context"
 )
 
-func Register(model *models.User, password string, ctx context.Context) (*models.User, *string, error) {
+func Register(model *models.User, password string, ctx context.Context) (*models.User, string, error) {
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return nil, nil, err
+		return nil, "", err
 	}
 	passwordHashString := string(passwordHash)
 	model.PasswordHash = &passwordHashString
 
 	if err = repositories.RegisterUser(model, ctx); err != nil {
-		return nil, nil, err
+		return nil, "", err
 	}
 
-	now := time.Now()
-
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, &dtos.TokenClaims{
-		Username: *model.Username,
-		RegisteredClaims: jwt.RegisteredClaims{
-			Issuer:    "goduit",
-			Subject:   *model.Email,
-			ExpiresAt: jwt.NewNumericDate(now.Add(10 * time.Minute)),
-			NotBefore: jwt.NewNumericDate(now),
-			IssuedAt:  jwt.NewNumericDate(now),
-			ID:        uuid.NewString(),
-		},
-	})
-
-	tokenString, err := token.SignedString(encryptionkeys.PrivateKey)
+	tokenString, err := identity.GenerateToken(model.Username, model.Email)
 	if err != nil {
-		return nil, nil, err
+		return nil, "", err
 	}
 
-	return model, &tokenString, nil
+	return model, tokenString, nil
 }
