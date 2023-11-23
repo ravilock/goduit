872edde4a42159d9bed62864ea910b69b2e733b6
ravime <ravime@g.globo>
2023-11-23 00:16:21 -0300

    feat: add getProfile request

---
+++ internal/profileManager/requests/getProfile.go
@@ -0,0 +1,24 @@
+package requests
+
+import (
+	"errors"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/ravilock/goduit/api/validators"
+)
+
+type GetProfile struct {
+	Username string `validate:"required,notblank,min=5,max=255"`
+}
+
+func (r *GetProfile) validate() error {
+	if err := validators.Validate.Struct(r); err != nil {
+		if validationErrors := new(validator.ValidationErrors); errors.As(err, validationErrors) {
+			for _, validationError := range *validationErrors {
+				return validators.ToHTTP(validationError)
+			}
+		}
+		return err
+	}
+	return nil
+}
