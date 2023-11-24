abcaa923c382b9fec1a47c52c313334c545d3f79
ravime <ravime@g.globo>
2023-11-24 02:05:28 -0300

    feat: add get article handler and test

---
+++ internal/articlePublisher/handlers/getArticle.go
@@ -0,0 +1,69 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/ravilock/goduit/api"
+	"github.com/ravilock/goduit/internal/app"
+	"github.com/ravilock/goduit/internal/articlePublisher/assemblers"
+	"github.com/ravilock/goduit/internal/articlePublisher/models"
+	"github.com/ravilock/goduit/internal/articlePublisher/requests"
+	profileManagerAssembler "github.com/ravilock/goduit/internal/profileManager/assemblers"
+	profileManager "github.com/ravilock/goduit/internal/profileManager/handlers"
+)
+
+type articleGetter interface {
+	GetArticleBySlug(ctx context.Context, slug string) (*models.Article, error)
+}
+
+type isFollowedChecker interface {
+	IsFollowedBy(ctx context.Context, followed, following string) bool
+}
+
+type getArticleHandler struct {
+	service         articleGetter
+	profileManager  profileManager.ProfileGetter
+	followerCentral isFollowedChecker
+}
+
+func (h *getArticleHandler) GetArticle(c echo.Context) error {
+	clientUsername := c.Request().Header.Get("Goduit-Client-Username")
+	request := new(requests.GetArticle)
+
+	request.Slug = c.Param("slug")
+	if err := request.Validate(); err != nil {
+		return err
+	}
+
+	ctx := c.Request().Context()
+
+	article, err := h.service.GetArticleBySlug(ctx, request.Slug)
+	if err != nil {
+		if appError := new(app.AppError); errors.As(err, &appError) {
+			switch appError.ErrorCode {
+			case app.ArticleNotFoundErrorCode:
+				return api.ArticleNotFound(request.Slug)
+			}
+		}
+		return err
+	}
+
+	author, err := h.profileManager.GetProfileByUsername(ctx, *article.Author)
+	if err != nil {
+		return err
+	}
+
+	isFollowing := h.followerCentral.IsFollowedBy(ctx, *author.Username, clientUsername)
+
+	authorProfile, err := profileManagerAssembler.ProfileResponse(author, isFollowing)
+	if err != nil {
+		return err
+	}
+
+	response := assemblers.ArticleResponse(article, authorProfile)
+
+	return c.JSON(http.StatusOK, response)
+}
