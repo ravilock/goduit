2fa5bf4e5ffe243971727542de977305483cac54
ravilock <ravi.me.professional@hotmail.com>
2023-11-24 19:44:26 -0300

    ref: add locally declared interface for isFollowerChecker

--- internal/profileManager/handlers/getProfile.go
+++ internal/profileManager/handlers/getProfile.go
@@ -13,13 +13,17 @@ import (
 	"github.com/ravilock/goduit/internal/profileManager/requests"
 )
 
-type IsFollowedChecker interface {
+type IsFollowedChecker interface { // TODO: remove when done
+	IsFollowedBy(ctx context.Context, followed, following string) bool
+}
+
+type isFollowedChecker interface {
 	IsFollowedBy(ctx context.Context, followed, following string) bool
 }
 
 type getProfileHandler struct {
 	service         profileGetter
-	followerCentral IsFollowedChecker
+	followerCentral isFollowedChecker
 }
 
 func (h *getProfileHandler) GetProfile(c echo.Context) error {
