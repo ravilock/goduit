13946d137562b8f79ff4d44205ebdb5f366d6be0
ravime <ravime@g.globo>
2023-12-15 19:21:37 -0300

    test: update profile request tests

--- internal/profileManager/requests/updateProfile_test.go
+++ internal/profileManager/requests/updateProfile_test.go
@@ -3,6 +3,7 @@ package requests
 import (
 	"testing"
 
+	"github.com/ravilock/goduit/api"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,7 +13,111 @@ func TestUpdateProfile(t *testing.T) {
 		err := request.Validate()
 		assert.NoError(t, err)
 	})
-	// TODO: add checks for theses values as they are optional and can be empty
+
+	t.Run("Email is required", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Email = ""
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Email").Error())
+	})
+
+	t.Run("Username is required", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Username = ""
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Username").Error())
+	})
+
+	t.Run("Bio is required", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Bio = ""
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Bio").Error())
+	})
+
+	t.Run("Image is required", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Image = ""
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Image").Error())
+	})
+
+	t.Run("Email should not be blank", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Email = " "
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Email").Error())
+	})
+
+	t.Run("Password should not be blank", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Password = " "
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Password").Error())
+	})
+
+	t.Run("Username should not be blank", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Username = " "
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Username").Error())
+	})
+
+	t.Run("Bio should not be blank", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Bio = " "
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Bio").Error())
+	})
+
+	t.Run("Image should not be blank", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Image = " "
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Image").Error())
+	})
+
+	t.Run("Password should contain at least 8 chars", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Password = "pass"
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.InvalidFieldLength("Password", "min", "8").Error())
+	})
+
+	t.Run("Username should contain at least 5 chars", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Username = "user"
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.InvalidFieldLength("Username", "min", "5").Error())
+	})
+
+	t.Run("Email should contain at most 256 chars", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Email = randomString(256) + "@hotmail.com"
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.InvalidFieldLength("Email", "max", "256").Error())
+	})
+
+	t.Run("Password should contain at most 72 chars", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Password = randomString(73)
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.InvalidFieldLength("Password", "max", "72").Error())
+	})
+
+	t.Run("Username should contain at most 255 chars", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Username = randomString(256)
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.InvalidFieldLength("Username", "max", "255").Error())
+	})
+
+	t.Run("Email should be a valid email", func(t *testing.T) {
+		request := generateUpdateProfileRequest()
+		request.User.Email = "email@hotmail."
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.InvalidFieldError("Email", request.User.Email).Error())
+	})
 }
 
 func generateUpdateProfileRequest() *UpdateProfile {
