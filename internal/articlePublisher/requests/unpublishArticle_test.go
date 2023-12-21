f3c4a7ab3f85a7bf02f9f9e5b4700b4b21c11c33
ravime <ravime@g.globo>
2023-12-21 00:55:24 -0300

    test: unpubllish article

---
+++ internal/articlePublisher/requests/unpublishArticle_test.go
@@ -0,0 +1,46 @@
+package requests
+
+import (
+	"testing"
+
+	"github.com/ravilock/goduit/api"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnpublishArticle(t *testing.T) {
+	t.Run("Valid request should not return errors", func(t *testing.T) {
+		request := generateUnpublishArticleRequest()
+		err := request.Validate()
+		assert.NoError(t, err)
+	})
+	t.Run("Slug is required", func(t *testing.T) {
+		request := generateUnpublishArticleRequest()
+		request.Slug = ""
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Slug").Error())
+	})
+	t.Run("Slug should not be blank", func(t *testing.T) {
+		request := generateUnpublishArticleRequest()
+		request.Slug = " "
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.RequiredFieldError("Slug").Error())
+	})
+	t.Run("Slug should contain at least 5 chars", func(t *testing.T) {
+		request := generateUnpublishArticleRequest()
+		request.Slug = "1234"
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.InvalidFieldLength("Slug", "min", "5").Error())
+	})
+	t.Run("Slug should contain at most 255 chars", func(t *testing.T) {
+		request := generateUnpublishArticleRequest()
+		request.Slug = randomString(256)
+		err := request.Validate()
+		assert.ErrorContains(t, err, api.InvalidFieldLength("Slug", "max", "255").Error())
+	})
+}
+
+func generateUnpublishArticleRequest() *UnpublishArticle {
+	return &UnpublishArticle{
+		Slug: "test-slug",
+	}
+}
