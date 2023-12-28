056f4fb345c0d324b36c14c8d4f5cfe2e695e5a1
ravilock <ravi.me.professional@hotmail.com>
2023-12-28 00:25:58 -0300

    test: remove moved tests

--- api/handlers/main_test.go
+++ api/handlers/main_test.go
@@ -1,11 +1,19 @@
 package handlers
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
+	"github.com/labstack/echo/v4"
+	"github.com/ravilock/goduit/api/requests"
 	"github.com/ravilock/goduit/api/validators"
 	encryptionkeys "github.com/ravilock/goduit/internal/config/encryptionKeys"
 	"github.com/ravilock/goduit/internal/config/mongo"
@@ -72,4 +80,85 @@ func clearDatabase() {
 			log.Fatal("Could not clear database", err)
 		}
 	}
+}
+
+func registerUser(username, email, password string) error {
+	if username == "" {
+		username = "default-username"
+	}
+	if email == "" {
+		email = "default.email@test.test"
+	}
+	if password == "" {
+		password = "default-password"
+	}
+	registerRequest := new(requests.Register)
+	registerRequest.User.Username = username
+	registerRequest.User.Email = email
+	registerRequest.User.Password = password
+	requestBody, err := json.Marshal(registerRequest)
+	if err != nil {
+		return err
+	}
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/users", bytes.NewBuffer(requestBody))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	if err := Register(c); err != nil {
+		return err
+	}
+	return nil
+}
+
+func followUser(followed, follower string) error {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/%s/follow", followed), nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetParamNames("username")
+	c.SetParamValues(followed)
+	req.Header.Set("Goduit-Client-Username", follower)
+	return Follow(c)
+}
+
+func makeSlug(title string) string {
+	loweredTitle := strings.ToLower(title)
+	titleWords := strings.Split(loweredTitle, " ")
+	return strings.Join(titleWords, "-")
+}
+
+func createArticle(title, description, body, authorUsername string, tagList []string) error {
+	if title == "" {
+		title = "Default Title"
+	}
+	if description == "" {
+		description = "Default Description"
+	}
+	if body == "" {
+		body = "Default Body"
+	}
+	if len(tagList) == 0 {
+		tagList = []string{"default-tag", "test"}
+	}
+	request := new(requests.CreateArticle)
+	request.Article.Title = title
+	request.Article.Description = description
+	request.Article.Body = body
+	request.Article.TagList = tagList
+	requestBody, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/api/articles", bytes.NewBuffer(requestBody))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.Header.Set("Goduit-Client-Username", authorUsername)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	if err := CreateArticle(c); err != nil {
+		return err
+	}
+	return nil
 }
