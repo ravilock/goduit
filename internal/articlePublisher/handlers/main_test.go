84d4f13f24475973180e2050c3623d5454e2e051
ravilock <ravi.me.professional@hotmail.com>
2023-12-28 00:26:00 -0300

    test: add update article test and fix update result decoding

--- internal/articlePublisher/handlers/main_test.go
+++ internal/articlePublisher/handlers/main_test.go
@@ -1,13 +1,22 @@
 package handlers
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
+	"github.com/labstack/echo/v4"
 	"github.com/ravilock/goduit/api/validators"
+	articlePublisherRequests "github.com/ravilock/goduit/internal/articlePublisher/requests"
 	encryptionkeys "github.com/ravilock/goduit/internal/config/encryptionKeys"
+	profileManagerModels "github.com/ravilock/goduit/internal/profileManager/models"
+	profileManager "github.com/ravilock/goduit/internal/profileManager/services"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -63,4 +72,57 @@ func clearDatabase(client *mongo.Client) {
 			log.Fatal("Could not clear database", err)
 		}
 	}
+}
+
+func registerUser(username, email, password string, manager *profileManager.ProfileManager) (string, error) {
+	if username == "" {
+		username = "default-username"
+	}
+	if email == "" {
+		email = "default.email@test.test"
+	}
+	if password == "" {
+		password = "default-password"
+	}
+	return manager.Register(context.Background(), &profileManagerModels.User{Username: &username, Email: &email}, password)
+}
+
+func makeSlug(title string) string {
+	loweredTitle := strings.ToLower(title)
+	titleWords := strings.Split(loweredTitle, " ")
+	return strings.Join(titleWords, "-")
+}
+
+func createArticle(title, description, body, authorUsername string, tagList []string, handler writeArticleHandler) error {
+	if title == "" {
+		title = "Default Title"
+	}
+	if description == "" {
+		description = "Default Description"
+	}
+	if body == "" {
+		body = "Default Body"
+	}
+	if len(tagList) == 0 {
+		tagList = []string{"default-tag", "test"}
+	}
+	request := new(articlePublisherRequests.WriteArticle)
+	request.Article.Title = title
+	request.Article.Description = description
+	request.Article.Body = body
+	request.Article.TagList = tagList
+	requestBody, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/api/articles", bytes.NewBuffer(requestBody))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.Header.Set("Goduit-Client-Username", authorUsername)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	if err := handler.WriteArticle(c); err != nil {
+		return err
+	}
+	return nil
 }
