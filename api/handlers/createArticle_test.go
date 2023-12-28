af52289de51fc2e1c8399ce29d0d2585fd195963
ravilock <ravi.me.professional@hotmail.com>
2023-12-28 00:25:58 -0300

    test: create article test

---
+++ api/handlers/createArticle_test.go
@@ -0,0 +1,72 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/ravilock/goduit/api/requests"
+	"github.com/ravilock/goduit/api/responses"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateArticle(t *testing.T) {
+	const createArticleTestUsername = "create-article-test-username"
+	const createArticleTestEmail = "create.article.test@test.test"
+
+	clearDatabase()
+	if err := registerAccount(createArticleTestUsername, createArticleTestEmail, ""); err != nil {
+		log.Fatal("Could not create user", err)
+	}
+
+	e := echo.New()
+	t.Run("Should create an article", func(t *testing.T) {
+		createArticleRequest := generateCreateArticleBody()
+		requestBody, err := json.Marshal(createArticleRequest)
+		assert.NoError(t, err)
+		req := httptest.NewRequest(http.MethodPost, "/api/articles", bytes.NewBuffer(requestBody))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		req.Header.Set("Goduit-Client-Username", createArticleTestUsername)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+		err = CreateArticle(c)
+		assert.NoError(t, err)
+		if rec.Code != http.StatusCreated {
+			t.Errorf("Got status different than %v, got %v", http.StatusCreated, rec.Code)
+		}
+		createArticleResponse := new(responses.Article)
+		err = json.Unmarshal(rec.Body.Bytes(), createArticleResponse)
+		checkCreateArticleResponse(t, createArticleRequest, createArticleTestUsername, createArticleResponse)
+		assert.NoError(t, err)
+	})
+}
+
+func generateCreateArticleBody() *requests.CreateArticle {
+	request := new(requests.CreateArticle)
+	request.Article.Title = "Test Article Name"
+	request.Article.Description = "Test Article Description"
+	request.Article.Body = "Test Article Body"
+	request.Article.TagList = []string{"test"}
+	return request
+}
+
+func checkCreateArticleResponse(t *testing.T, request *requests.CreateArticle, author string, response *responses.Article) {
+	t.Helper()
+	assert.Equal(t, request.Article.Title, response.Article.Title, "Wrong article title")
+	assert.Equal(t, request.Article.Description, response.Article.Description, "Wrong article description")
+	assert.Equal(t, request.Article.Body, response.Article.Body, "Wrong article body")
+	assert.Equal(t, makeSlug(request.Article.Title), response.Article.Slug, "Wrong article body")
+	assert.Equal(t, request.Article.TagList, response.Article.TagList, "Wrong article body")
+	assert.Equal(t, author, response.Article.Author.Username, "Wrong article author username")
+}
+
+func makeSlug(title string) string {
+	loweredTitle := strings.ToLower(title)
+	titleWords := strings.Split(loweredTitle, " ")
+	return strings.Join(titleWords, "-")
+}
