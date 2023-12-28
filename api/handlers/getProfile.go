5a962d520e6c7d887a96e86896ae70a3a779f5b4
ravilock <ravi.me.professional@hotmail.com>
2023-12-28 00:25:57 -0300

    feat: add username validation on getProfile function

--- api/handlers/getProfile.go
+++ api/handlers/getProfile.go
@@ -6,21 +6,30 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"github.com/ravilock/goduit/api/assemblers"
+	"github.com/ravilock/goduit/api/requests"
+	"github.com/ravilock/goduit/api/validators"
 	"github.com/ravilock/goduit/internal/app/dtos"
 	"github.com/ravilock/goduit/internal/app/services"
 )
 
 func GetProfile(c echo.Context) error {
 	var subject string
-	username, tokenString := c.Param("username"), c.Get("user")
+	request := new(requests.GetProfile)
+
+	request.Username = c.Param("username")
+	if err := validators.GetProfile(request); err != nil {
+		return err
+	}
+
+	tokenString := c.Get("user")
 	if tokenString != "" {
 		claims := tokenString.(*jwt.Token).Claims.(*dtos.TokenClaims)
-		if username != claims.Username {
+		if request.Username != claims.Username {
 			subject = claims.Username
 		}
 	}
 
-	dto, err := services.GetProfileByUsername(username, subject, c.Request().Context())
+	dto, err := services.GetProfileByUsername(request.Username, subject, c.Request().Context())
 	if err != nil {
 		return err
 	}
