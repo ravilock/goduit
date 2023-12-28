4c377124b8987d5054ee39ab26a6e2c33b2cad16
ravilock <ravi.me.professional@hotmail.com>
2023-12-28 00:26:00 -0300

    test: article-slug from getArticle and unpublish article

--- internal/articlePublisher/requests/articleSlug_test.go
+++ internal/articlePublisher/requests/articleSlug_test.go
@@ -7,40 +7,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnpublishArticle(t *testing.T) {
+func TestArticleSlug(t *testing.T) {
 	t.Run("Valid request should not return errors", func(t *testing.T) {
-		request := generateUnpublishArticleRequest()
+		request := generateArticleSlugRequest()
 		err := request.Validate()
 		assert.NoError(t, err)
 	})
 	t.Run("Slug is required", func(t *testing.T) {
-		request := generateUnpublishArticleRequest()
+		request := generateArticleSlugRequest()
 		request.Slug = ""
 		err := request.Validate()
 		assert.ErrorContains(t, err, api.RequiredFieldError("Slug").Error())
 	})
 	t.Run("Slug should not be blank", func(t *testing.T) {
-		request := generateUnpublishArticleRequest()
+		request := generateArticleSlugRequest()
 		request.Slug = " "
 		err := request.Validate()
 		assert.ErrorContains(t, err, api.RequiredFieldError("Slug").Error())
 	})
 	t.Run("Slug should contain at least 5 chars", func(t *testing.T) {
-		request := generateUnpublishArticleRequest()
+		request := generateArticleSlugRequest()
 		request.Slug = "1234"
 		err := request.Validate()
 		assert.ErrorContains(t, err, api.InvalidFieldLength("Slug", "min", "5").Error())
 	})
 	t.Run("Slug should contain at most 255 chars", func(t *testing.T) {
-		request := generateUnpublishArticleRequest()
+		request := generateArticleSlugRequest()
 		request.Slug = randomString(256)
 		err := request.Validate()
 		assert.ErrorContains(t, err, api.InvalidFieldLength("Slug", "max", "255").Error())
 	})
 }
 
-func generateUnpublishArticleRequest() *UnpublishArticle {
-	return &UnpublishArticle{
+func generateArticleSlugRequest() *ArticleSlug {
+	return &ArticleSlug{
 		Slug: "test-slug",
 	}
 }
