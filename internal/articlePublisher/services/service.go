a6aaddcce52922a0c8d11cbee5ea561209cb5d0b
ravilock <ravi.me.professional@hotmail.com>
2023-12-28 00:25:59 -0300

    feat: implement unpublish article service

--- internal/articlePublisher/services/service.go
+++ internal/articlePublisher/services/service.go
@@ -5,10 +5,12 @@ import "github.com/ravilock/goduit/internal/articlePublisher/repositories"
 type ArticlePublisher struct {
 	writeArticleService
 	getArticleService
+	unpublishArticleService
 }
 
 func NewArticlePublisher(articleRepository *repositories.ArticleRepository) *ArticlePublisher {
 	write := writeArticleService{articleRepository}
 	get := getArticleService{articleRepository}
-	return &ArticlePublisher{write, get}
+	unpublish := unpublishArticleService{articleRepository}
+	return &ArticlePublisher{write, get, unpublish}
 }
