147563ff55be937519dcef520884fc750aae56e5
ravilock <ravi.me.professional@hotmail.com>
2024-01-27 02:08:53 -0300

    feat: implement comment assembler

---
+++ internal/articlePublisher/assemblers/comment.go
@@ -0,0 +1,19 @@
+package assemblers
+
+import (
+	"github.com/ravilock/goduit/internal/articlePublisher/models"
+	"github.com/ravilock/goduit/internal/articlePublisher/responses"
+	profileManagerResponses "github.com/ravilock/goduit/internal/profileManager/responses"
+)
+
+func CommentResponse(comment *models.Comment, author *profileManagerResponses.ProfileResponse) *responses.CommentResponse {
+	response := new(responses.CommentResponse)
+	response.Comment.ID = comment.ID.Hex()
+	response.Comment.Body = *comment.Body
+	response.Comment.CreatedAt = *comment.CreatedAt
+	if comment.UpdatedAt != nil {
+		response.Comment.UpdatedAt = *comment.UpdatedAt
+	}
+	response.Comment.Author = author.Profile
+	return response
+}
