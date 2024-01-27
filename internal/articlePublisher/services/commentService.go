cd1151a1c62ff48c966e1e5744b23bf9ed9cd4b9
ravilock <ravi.me.professional@hotmail.com>
2024-01-27 01:09:22 -0300

    export list comment service

--- internal/articlePublisher/services/commentService.go
+++ internal/articlePublisher/services/commentService.go
@@ -4,9 +4,11 @@ import "github.com/ravilock/goduit/internal/articlePublisher/repositories"
 
 type CommentPublisher struct {
 	writeCommentService
+	listCommentService
 }
 
 func NewCommentPublisher(commentRepository *repositories.CommentRepository) *CommentPublisher {
 	write := writeCommentService{commentRepository}
-	return &CommentPublisher{write}
+	list := listCommentService{commentRepository}
+	return &CommentPublisher{write, list}
 }
