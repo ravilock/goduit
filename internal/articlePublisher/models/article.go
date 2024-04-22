0ce1525d4b5c27b90724c51d296d8d86d71a657a
ravilock <ravi.me.professional@hotmail.com>
2024-04-22 12:47:10 -0300

    feat: add json tags for article model

--- internal/articlePublisher/models/article.go
+++ internal/articlePublisher/models/article.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Article struct {
-	ID             *primitive.ObjectID `bson:"_id,omitempty"`
-	Author         *string             `bson:"author,omitempty"`
-	Slug           *string             `bson:"slug,omitempty"`
-	Title          *string             `bson:"title,omitempty"`
-	Description    *string             `bson:"description,omitempty"`
-	Body           *string             `bson:"body,omitempty"`
-	TagList        []string            `bson:"tagList,omitempty"`
-	CreatedAt      *time.Time          `bson:"createdAt,omitempty"`
-	UpdatedAt      *time.Time          `bson:"updatedAt,omitempty"`
-	FavoritesCount *int64              `bson:"favoritesCount,omitempty"`
+	ID             *primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Author         *string             `bson:"author,omitempty" json:"author,omitempty"`
+	Slug           *string             `bson:"slug,omitempty" json:"slug,omitempty"`
+	Title          *string             `bson:"title,omitempty" json:"title,omitempty"`
+	Description    *string             `bson:"description,omitempty" json:"description,omitempty"`
+	Body           *string             `bson:"body,omitempty" json:"body,omitempty"`
+	TagList        []string            `bson:"tagList,omitempty" json:"tagList,omitempty"`
+	CreatedAt      *time.Time          `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UpdatedAt      *time.Time          `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
+	FavoritesCount *int64              `bson:"favoritesCount,omitempty" json:"favoritesCount,omitempty"`
 }
